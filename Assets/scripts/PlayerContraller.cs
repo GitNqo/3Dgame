af92bb3d37e88f158a78c852e2230b694af17a85
小田 真 <odanao.1001@gmail.com>
2023-05-19 11:33:16 +0900

    player1回目

--- Assets/scripts/PlayerContraller.cs
+++ Assets/scripts/PlayerContraller.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerContraller : MonoBehaviour
-{
+{ 
+     public float speed = 20; // 動く速さ
+
+private Rigidbody rb; // Rididbody
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
     }
 }
