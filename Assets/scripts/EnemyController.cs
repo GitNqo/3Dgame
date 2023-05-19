d211abf6e899eb871aa629b0216b487386fb0506
小田 真 <odanao.1001@gmail.com>
2023-05-19 11:53:55 +0900

    player ３回目

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject player; // 玉のオブジェクト
+
+    private Vector3 offset; // 玉からカメラまでの距離
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
